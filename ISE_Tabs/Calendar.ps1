21206a5e877b30756127580d2d91cc6ad1d2f337
Ariel Hart <merbarbie1025@yahoo.com>
2015-07-27 15:49:42 -0700

    Small additions

---
+++ ISE_Tabs/Calendar.ps1
@@ -0,0 +1,31 @@
+﻿#Calendar
+
+##Calendar permissions-
+
+$Owner = write arik.prawer@colonyamerican.com:\Calendar
+$Requestor = write amy.steiner@colonyamerican.com 
+
+add-MailboxFolderPermission -Identity $Owner -User $Requestor -AccessRights PublishingAuthor
+
+##Access Levels-
+Owner, PublishingEditor, Editor, PublishingAuthor, Author, NonEditingAuthor, Reviewer, Contributor, AvailabilityOnly, LimitedDetails
+
+Remove-MailboxFolderPermission -Identity $Owner -user $Requestor
+
+##View Permissions-
+Get-MailboxFolderPermission –Identity $owner
+
+Get-MailboxFolderPermission –Identity Dana.dunn@colonyamerican.com:\Calendar | FT User, AccessRights -AutoSize
+
+#Calendar Manipulation
+
+Calendar permissions-
+add-MailboxFolderPermission -Identity brad.hull@colonyamerican.com:\Calendar -User Melissa.Ferris@colonyamerican.com -AccessRights PublishingAuthor
+
+##Access Levels-
+##Owner PublishingEditor, Editor, PublishingAuthor, Author, NonEditingAuthor, Reviewer, Contributor, AvailabilityOnly, LimitedDetails
+
+Remove-MailboxFolderPermission -Identity user@mycompany:\calendar -user myuser@mycompany.com
+
+#View Permissions-
+Get-MailboxFolderPermission –Identity ariel.hart@colonyamerican.com:\calendar
---
+++ ISE_Tabs/Mailboxes.ps1
@@ -0,0 +1,19 @@
+﻿#Add rights to a mailbox
+
+$MBX = write carlos.quintanilla@colonyamerican.com  
+$MUser = write  pamela.kates@colonyamerican.com 
+
+Get-Mailbox $MBX |
+    Add-MailboxPermission -User $MUser -AccessRights FullAccess -InheritanceType All |
+    Add-RecipientPermission -AccessRights SendAs -Trustee $MUser -Confirm:$false
+
+#Remove rights to a mailbox
+$RMBX = write Neerali.shukla@Colonyamerican.com 
+$RMUser = write ariel.hart@colonyamerican.com
+
+Remove-MailboxPermission -Identity $RMBX -User $RMUser -AccessRights FullAccess -InheritanceType All -Confirm:$false
+
+#Set Primary email address
+
+Set-Mailbox Leah.granovskaya@colonyamerican.onmicrosoft.com -EmailAddress SMTP:Leah.granovskaya@colonyamericanfinance.com
+
