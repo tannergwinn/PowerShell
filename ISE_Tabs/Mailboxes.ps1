786c01d8c28d725a963903c733da5aada07488ae
Ariel Hart <merbarbie1025@yahoo.com>
2015-07-28 16:00:10 -0700

    Commit

--- ISE_Tabs/Mailboxes.ps1
+++ ISE_Tabs/Mailboxes.ps1
@@ -1,14 +1,14 @@
 ﻿#Add rights to a mailbox
 
-$MBX = write carlos.quintanilla@colonyamerican.com  
-$MUser = write  pamela.kates@colonyamerican.com 
+$MBX = write @colonyamerican.com  
+$MUser = write  ariel.hart@colonyamerican.com 
 
 Get-Mailbox $MBX |
     Add-MailboxPermission -User $MUser -AccessRights FullAccess -InheritanceType All |
     Add-RecipientPermission -AccessRights SendAs -Trustee $MUser -Confirm:$false
 
 #Remove rights to a mailbox
-$RMBX = write Neerali.shukla@Colonyamerican.com 
+$RMBX = write leah.granovskaya@colonyamericanfinance.com 
 $RMUser = write ariel.hart@colonyamerican.com
 
 Remove-MailboxPermission -Identity $RMBX -User $RMUser -AccessRights FullAccess -InheritanceType All -Confirm:$false
