3954b91b73de7156289d82a7fb55e6baea3e7770
Ashart1025 <Merbarbie1025@yahoo.com>
2015-10-28 13:45:53 -0700

    10/28/15 CopyDir

--- ISE_Tabs/CopyDir+FilesFromList.ps1
+++ ISE_Tabs/CopyDir+FilesFromList.ps1
@@ -2,5 +2,8 @@
 
 Foreach ($folder in $Folders)
 {
-ROBOCOPY c:\ScriptSources\$folder c:\temp\$folder
+ROBOCOPY "c:\SFN Docs\$folder" *_Title* c:\temp\Title
+ROBOCOPY "c:\SFN Docs\$folder" *_Deed* c:\temp\Deed 
+ROBOCOPY "c:\SFN Docs\$folder" *_PSA* c:\temp\PSA
+ROBOCOPY "c:\SFN Docs\$folder" *_PurchasePriceAllocation* c:\temp\PPA
 }
