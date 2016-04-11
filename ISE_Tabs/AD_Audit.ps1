38f92d6e924561be4db0cd42c4618c7a8b86f7c6
Ashart1025 <Merbarbie1025@yahoo.com>
2016-04-11 16:00:41 -0700

    Audit Scripts 04/11/16
    Created scripts for Sox / Yardi user audits

--- ISE_Tabs/AD_Audit.ps1
+++ ISE_Tabs/AD_Audit.ps1
@@ -1,3 +1,12 @@
-﻿Get-ADUser -filter *  -properties passwordlastset, LastLogonTimestamp, Whencreated, DistinguishedName, Whenchanged  |
+﻿
+##Sox Audit
+Get-ADUser -filter *  -properties passwordlastset, LastLogonTimestamp, Whencreated, DistinguishedName, Whenchanged  |
     Select-object Name, passwordlastset, @{n='LastLogonTimestamp';e={[DateTime]::FromFileTime($_.LastLogonTimestamp)}}, Whencreated, whenchanged, DistinguishedName |
     Export-csv -path c:\ScriptsOutput\ADAccess_$((Get-Date).ToString('MM-dd-yyyy_hh-mm-ss')).csv
+
+
+#Yardi User Audit
+
+Get-ADUser -filter *  -properties GivenName, Surname, Userprincipalname, title, physicalDeliveryOfficeName -SearchBase "OU=CAH_Users,DC=colonyah,DC=local" |
+    Select-object GivenName, Surname, Userprincipalname, title, physicalDeliveryOfficeName |
+    Export-csv -path c:\ScriptsOutput\YardiAudit_$((Get-Date).ToString('MM-dd-yyyy_hh-mm-ss')).csv
