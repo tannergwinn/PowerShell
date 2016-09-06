838f94020a2338c9426b6116584d6324db6ccb11
Ariel Hart <merbarbie1025@yahoo.com>
2016-09-06 09:02:57 -0700

    Commit 09.06.16_2
    Breakout of O365 commands

---
+++ ISE_Tabs/O365RemoveUsers.ps1
@@ -0,0 +1,27 @@
+﻿#Removing O365 Users
+
+#Removing single users from O365 (license already removed)
+
+$User = "Katie.Day@colonystarwood.com"
+    Get-MsolUser -UserPrincipalName $User
+    #Set-MsolUserLicense -UserPrincipalName $user -RemoveLicenses Colonyamerican:StandardPACK, Colonyamerican:CRMSTANDARD
+    Remove-MsolUser -UserPrincipalName $User -Force
+    #Remove-MsolUser -UserPrincipalName $User -RemoveFromRecyclebin -Force
+
+#Batch users from O365 -License already removed (Uses email address)
+
+$users = get-content "C:\ScriptSources\RemoveFromO365.csv"
+foreach ($user in $users)
+{
+Get-MsolUser -UserPrincipalName $user
+Remove-MsolUser -UserPrincipalName $user
+}
+
+#Single / individual user removal from 0365 with license -Removes from RecycleBin
+
+$user = Read-Host "Enter email"
+
+Get-MsolUser -UserPrincipalName $user
+    Set-MsolUserLicense -UserPrincipalName $user -RemoveLicenses Colonyamerican:STANDARDPACK
+    Remove-MsolUser -UserPrincipalName $user -Force
+    Remove-MsolUser -UserPrincipalName $user -RemoveFromRecyclebin
