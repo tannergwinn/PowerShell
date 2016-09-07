44ad9b2237019ecdc211ae2a7d5140d1e6ffa6e9
Ariel Hart <merbarbie1025@yahoo.com>
2016-09-07 12:19:08 -0700

    Commit 09.07.16
    O365 message trace script

---
+++ ISE_Tabs/O365MessageTrace.ps1
@@ -0,0 +1,14 @@
+﻿
+
+#O365 Message Trace 
+
+Get-MessageTrace -SenderAddress Scott.Snider@waypointhomes.com -StartDate “08/31/16 21:00” -EndDate “09/06/16 22:00” | Select-Object  MessageID, Date, Event, Action, Detail, Data, Received, SenderAddress, RecipientAddress, Subject, Status, ToIP, FromIP, Size,  MessageTraceID | Export-Csv C:\ScriptOutput\Scott.SniderMessageTrace.csv
+
+
+#48 hour trace
+$dateEnd = get-date 
+$dateStart = $dateEnd.AddHours(-48)
+Get-MessageTrace -StartDate $dateStart -EndDate $dateEnd | Select-Object Received, SenderAddress, RecipientAddress, Subject, Status, ToIP, FromIP, Size, MessageID, MessageTraceID | Export-Csv C:\ScriptOutput\48HourTest.csv
+
+
+
