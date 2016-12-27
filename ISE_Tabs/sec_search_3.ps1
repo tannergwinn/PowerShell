d836c06234939c9169273c9aa4320927d5957cb2
Ariel Hart <merbarbie1025@yahoo.com>
2016-12-27 11:51:51 -0700

    12.27.16 Added File Search / Copy scripts from JW

---
+++ ISE_Tabs/sec_search_3.ps1
@@ -0,0 +1,35 @@
+# This script, created by Jason Weitzman, should search the egnyte local storage and find documents fitting the following critera
+
+# 1: document must be a lease
+
+# 2: document must be in a folder with the matching address from CSV.
+
+
+
+#path vars
+
+$CSVPath = "\\10.1.103.10\Users\Public\Documents\Dropbox\wpscripting\OSN (09-18-2014) codeOnly_AfterDrop.csv";
+
+$SearchDir = "\\10.1.50.50\Capital_Markets\Due Dili\SWAY StructuredFN\Uploads in SFN";
+
+$DestDir = "\\10.1.50.50\Capital_Markets\Securities Results";
+
+
+
+#Import CSV
+
+$Codes = Get-Content $CSVPath;
+
+
+
+#Iterate through CSV lines and perform searc/copy of all lease pdf files found with the same address.
+
+Foreach ($Code in $Codes){
+
+write-output "`nCode from CSV = $Code" >> $DestDir\log.txt;
+
+write-host "`nCode from CSV = $Code";
+
+Get-ChildItem $SearchDir\* -recurse -filter *$Code* -Include "*finalstatement*.pdf" | Copy-Item -Destination $DestDir;
+
+}
